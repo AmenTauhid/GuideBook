60508ba5dfa10d3955e07cb46b1994cd2a75b132
Ayman Tauhid <aymantauhid2@gmail.com>
2023-02-11 17:59:34 -0500

    Able to open four application by user input, i.e., if the user inputs a question or statement, if it has the keyword, it will open the app accordingly.

--- app.py
+++ app.py
@@ -12,53 +12,21 @@ def find_chrome():
     except WindowsError:
         return None
 
-# def find_exe(exe_name):
-#     if platform.system() == "Windows":
-#         try:
-#             reg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-#             key = winreg.OpenKey(reg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe")
-#             path = winreg.QueryValue(key, None)
-#             winreg.CloseKey(key)
-#             return path
-#         except WindowsError:
-#             return None
-#     elif platform.system() == "Darwin":
-#         try:
-#             path = subprocess.check_output(["mdfind", exe_name]).strip().decode("utf-8")
-#         except subprocess.CalledProcessError:
-#             return None
-#     else:
-#         return None
-#     return path
-
 print("Welcome to Application Runner!")
-print("Available applications:")
-print("1. Notepad")
-print("2. Calculator")
-print("3. Paint")
-print("4. Chrome")
 
-app = int(input("Enter the number of the application you want to run: "))
+question = input("Ask me to open an application: ")
 
-if app == 1:
+if "notepad" in question:
     subprocess.Popen(["notepad.exe"])
-elif app == 2:
+elif "calculator" in question:
     subprocess.Popen(["calc.exe"])
-elif app == 3:
-    import subprocess
+elif "paint" in question:
     subprocess.Popen(["mspaint.exe"])
-elif app == 4:
+elif "chrome" in question:
     chrome_path = find_chrome()
     if chrome_path:
         subprocess.Popen([chrome_path])
     else:
         print("Google Chrome not found.")
-    # chrome_path = find_exe("Google Chrome.app")
-    # if chrome_path is not None:
-    #     subprocess.Popen(["open", "-a", chrome_path])
-    # else:
-    #     print("Google Chrome could not be found.")
 else:
-    print("Invalid option. Please enter a number between 1 and 3.")
-    
-    
+    print("I am sorry, I could not find the application you requested.")
